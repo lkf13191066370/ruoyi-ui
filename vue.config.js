c06dd35b248b163719f4e5370876420c0e39cb6c
lkf13191066370 <297488660@qq.com>
2022-09-15 14:40:51 +0800

    shangti

--- vue.config.js
+++ vue.config.js
@@ -32,7 +32,7 @@ module.exports = {
     host: '0.0.0.0',
     port: port,
     open: true,
-    proxy: {
+    proxy: {0000
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
         target: `http://localhost:8081`,
